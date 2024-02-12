b1c837decc4312b4f11fd6f0e947296460403ff2
Ryan Pond <39492348+NUEXGUY@users.noreply.github.com~>
2024-02-12 01:07:34 -0600

    establish connection to PlanetScale Database

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config({ path: './agewise.env' });
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_NAME);
+
+const mysql = require('mysql2/promise');
+
+async function checkDatabaseConnection() {
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: true // This ensures that Node.js rejects unauthorized connections, for security
+      }
+    });
+
+    // This is a simple query that doesn't depend on any table
+    const [rows] = await connection.execute('SELECT 1;');
+    console.log('Connection to the PlanetScale database successful:', rows);
+    // Close the connection after the check
+    await connection.end();
+  } catch (error) {
+    console.error('Unable to connect to the PlanetScale database:', error);
+  }
+}
+
+// Call the function to test the connection
+checkDatabaseConnection();
