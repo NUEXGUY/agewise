a2eecb0981d9cea83c4cf77b90ce4d64f4442b41
Ryan Pond <39492348+NUEXGUY@users.noreply.github.com~>
2024-02-17 23:30:24 -0600

    adds SEO metadata to HTML and adds GTM

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const pool = mysql.createPool({
 
 const express = require('express')
 const app = express()
-
+app.use(express.static('public'))
 app.use(express.json())
 
 // Start the server
@@ -51,50 +51,6 @@ app.post('/submit-user-info', async (req, res) => {
       res.status(500).json({ success: false, message: 'Error submitting user info', error: error.message })
     }
   }
-})
-
-// Set up a route to serve the HTML content with the injected variables
-app.get('/', (req, res) => {
-  const gtag = process.env.GTAG
-
-  const bodyContent = `
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${gtag}"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-  `
-
-  const injectedScripts = `
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','${gtag}');</script>
-    <!-- End Google Tag Manager -->
-  `
-
-  const htmlContent = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>AgeWise - Chronic Pain Guide</title>
-      <link rel="stylesheet" href="style.css">
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Roboto+Condensed:wght@800&display=swap" rel="stylesheet">
-      <link rel="icon" href="favicon.svg" type="image/svg+xml">
-      ${injectedScripts}
-    </head>
-    <body>
-      ${bodyContent}
-    </body>
-    </html>
-  `
-
-  res.send(htmlContent)
 })
 
 const OpenAI = require('openai')
